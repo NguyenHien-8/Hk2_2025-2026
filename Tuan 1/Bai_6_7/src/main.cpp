98385036d8824064683abf9fd56affd9aa566632
NGUYENHIEN <trannguyenhien29085@gmail.com>
2025-12-31 19:18:32 +0700

    add code bai 6,7

---
+++ Tuan 1/Bai_6_7/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // digitalWrite(2, HIGH);
+  // delay(1000);
+  // digitalWrite(2, LOW);
+  // delay(1000);
+  Serial.println("Hello,World!");
+  delay(1000);
+}
